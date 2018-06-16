0f7af6278a0e99bf358c186ca493867b3c4af60f
Ankit Muchhala <muchhalaankit@gmail.com>
2018-06-17 00:07:29 +0530

    update examples and create docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
 
-const devConfig = {
+const docConfig = {
   devtool: 'eval-source-map',
   entry: {
-    main: './examples/js/index.js'
+    main: './examples/js/index.js',
   },
   output: {
-    filename: './dist/[name].js'
+    filename: './docs/[name].js',
   },
   module: {
     loaders: [
@@ -18,30 +18,34 @@ const devConfig = {
         test: /\.js$/,
         include: /(src|examples)/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './examples/index.html'
-    })
+      template: './examples/index.html',
+    }),
   ],
   resolve: {
     alias: {
       'react-16-dropdown': path.resolve(__dirname, 'src/Dropdown'),
-    }
-  }
+    },
+  },
 };
 
 const buildConfig = {
   entry: {
-    main: './src/Dropdown.js'
+    main: './src/Dropdown.js',
   },
   output: {
     filename: './dist/react-16-dropdown.js',
     library: 'react16Dropdown',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
     loaders: [
@@ -49,14 +53,38 @@ const buildConfig = {
         test: /\.js$/,
         include: /src/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   externals: [
     'react',
-    'react-dom'
-  ]
+    'react-dom',
+  ],
+};
+
+if (TARGET === 'build-docs') {
+  docConfig.devtool = undefined;
+  docConfig.plugins = [
+    new HtmlWebpackPlugin({
+      template: './examples/index.html',
+      filename: './docs/index.html',
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      output: {
+        ascii_only: true,
+      },
+      mangle: {
+        screw_ie8: true,
+      },
+      compress: {
+        screw_ie8: true,
+      },
+      comments: false,
+      sourceMap: false,
+    }),
+  ];
 }
 
-module.exports = TARGET === 'build' ? buildConfig : devConfig;
+module.exports = TARGET === 'build' ? buildConfig : docConfig;
