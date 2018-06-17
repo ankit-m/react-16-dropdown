2be0a0ad6c8501c5a6a9b2a0bd3c18d4b201979f
Ankit Muchhala <muchhalaankit@gmail.com>
2018-06-17 17:48:24 +0530

    add github and twitter buttons

--- examples/js/Jumbotron.js
+++ examples/js/Jumbotron.js
@@ -2,6 +2,30 @@ import React from 'react';
 
 import Dropdown from 'react-16-dropdown';
 
+function TwitterButton() {
+  return (
+    <a
+      href='https://twitter.com/ankit_muchhala?ref_src=twsrc%5Etfw'
+      className='twitter-follow-button'
+      data-show-count='false'
+    >
+      Follow @ankit_muchhala
+    </a>
+  );
+}
+
+function GithubButton() {
+  return (
+    <a
+      className='github-button'
+      href='https://github.com/ankit-m/react-16-dropdown'
+      aria-label='Star ankit-m/react-16-dropdown on GitHub'
+    >
+      GitHub
+    </a>
+  );
+}
+
 /**
  * Introductory component
  */
@@ -25,6 +49,11 @@ export default function Jumbotron() {
             <h1>react 16 dropdown</h1>
             <p className='text-secondary'>Zero-dependency, lightweight and fully cuztomizable dropdown (not select) for React.</p>
             <code>npm install --save react-16-dropdown</code>
+            <div className='mt-4'>
+              <TwitterButton />
+              &nbsp;&nbsp;
+              <GithubButton />
+            </div>
           </div>
           <div className='col-md-6'>
             <Dropdown
