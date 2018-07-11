3acb418561e1646a16226e6f6da00d9fc97e4105
Ankit Muchhala <muchhalaankit@gmail.com>
2018-07-11 10:27:53 +0530

    add jsdocs for trigger

--- src/Trigger.js
+++ src/Trigger.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * Default trigger renderer - Displays a plain button
+ * with label
+ *
+ * @param {Object} props - React props
+ * @param {Boolean} props.disabled - Trigger disabled
+ * @param {String|ReactElement} props.label - Trigger display label
+ */
 export function TriggerRenderer(props) {
   return (
     <button
@@ -11,6 +19,18 @@ export function TriggerRenderer(props) {
   );
 }
 
+/**
+ * Default trigger component - Renders a div with
+ * all the handlers
+ *
+ * @param {Object} props - React props
+ * @param {ReactElement} [props.renderer] - Custom trigger renderer
+ * @param {ReactRef} props.triggerRef - React ref for trigger
+ * @param {Function} props.onClick - Click handler
+ * @param {Function} props.onKeyDown - Key down handler
+ * @param {Boolean} props.disabled - Trigger disabled
+ * @param {String|ReactElement} props.label - Trigger display label
+ */
 export default function Trigger(props) {
   const Renderer = props.renderer || TriggerRenderer;
 
